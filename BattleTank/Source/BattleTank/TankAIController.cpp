54bf45d5b1ad4fa2a6012bcacf5430413715c1af
andreadago <and.dago@hotmail.it>
2017-02-08 11:07:33 +0100

    AIController GetPlayerTank() function

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -18,9 +18,30 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("AI Possessing %s tank"), *ControlledTank->GetName());
 	}
+
+	ATank* PlayerTank = GetPlayerTank();
+	if (PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s AIController found %s"), *GetName(), *PlayerTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s AIController cannot find PlayerTank"), *GetName(), *PlayerTank->GetName());
+	}
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	ATank* PlayerTank = nullptr;
+	APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (PlayerPawn)
+	{
+		PlayerTank = Cast<ATank>(PlayerPawn);		
+	}
+	return PlayerTank;
 }
