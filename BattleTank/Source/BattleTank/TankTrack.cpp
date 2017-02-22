f65a3e1b4672d6d5479be16ac5425200d6f0602e
andreadago <and.dago@hotmail.it>
2017-02-22 16:52:29 +0100

    TankTracks class and tank movement

---
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	FString Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle at %f"), *Name, Throttle);
+
+	FVector ForceApplied = GetForwardVector() * Throttle * TankMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
+
+
